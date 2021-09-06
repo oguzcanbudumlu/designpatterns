4ff9c0118ff57c7a2ef91e34fd2647e893579502
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-09-06 19:41:34 +0300

    one test added

---
+++ src/singleton/test2/Client.java
@@ -0,0 +1,7 @@
+package singleton.test2;
+
+public class Client {
+    public static void main(String[] args) {
+//        StackOverflow stackOverflow = new StackOverflow();
+    }
+}
---
+++ src/singleton/test2/StackOverflow.java
@@ -0,0 +1,9 @@
+package singleton.test2;
+
+public class StackOverflow {
+    StackOverflow stackOverflow = new StackOverflow();
+
+    StackOverflow() {
+
+    }
+}
